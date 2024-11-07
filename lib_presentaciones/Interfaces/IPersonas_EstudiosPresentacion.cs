1c5da8ce406385706f6e9f37d870aba4b2e4260f
ErianSerna <erianserna1114297@correo.itm.edu.co>
2024-11-07 17:19:36 -0500

    Agrego otras implementaciones-interfaces

---
+++ lib_presentaciones/Interfaces/IPersonas_EstudiosPresentacion.cs
@@ -0,0 +1,13 @@
+﻿using lib_entidades.Modelos;
+
+namespace lib_presentaciones.Interfaces
+{
+    public interface IPersonas_EstudiosPresentacion
+    {
+        Task<List<Personas_Estudios>> Listar();
+        Task<List<Personas_Estudios>> Buscar(Personas_Estudios entidad, string tipo);
+        Task<Personas_Estudios> Guardar(Personas_Estudios entidad);
+        Task<Personas_Estudios> Modificar(Personas_Estudios entidad);
+        Task<Personas_Estudios> Borrar(Personas_Estudios entidad);
+    }
+}
