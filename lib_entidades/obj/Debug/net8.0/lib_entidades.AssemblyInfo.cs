20d21d9918ba9b61c80a36ec64b564bb346870f5
EmmanuelCadavid <emmanuelcadavid1117639@correo.itm.edu.co>
2024-11-07 18:50:16 -0500

    Se añade el Startup

--- lib_entidades/obj/Debug/net8.0/lib_entidades.AssemblyInfo.cs
+++ lib_entidades/obj/Debug/net8.0/lib_entidades.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lib_entidades")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33b9bd86a8d038a14ff1dbd406b16249c0f1607a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3290163919a8f510049a097dc92fba70829978b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("lib_entidades")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lib_entidades")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- lib_presentaciones/obj/Debug/net8.0/lib_presentaciones.AssemblyInfo.cs
+++ lib_presentaciones/obj/Debug/net8.0/lib_presentaciones.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lib_presentaciones")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b0dd2016b52bff88c94b4121d746ea5ee49c315")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3290163919a8f510049a097dc92fba70829978b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("lib_presentaciones")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lib_presentaciones")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
