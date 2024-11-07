da5446e9b6c246106cb8d5cbf36ba9722027be23
ErianSerna <erianserna1114297@correo.itm.edu.co>
2024-11-07 16:51:20 -0500

    Creacion de la carpeta interfaces

---
+++ lib_presentaciones/Implementanciones/CargosPresentacion.cs
@@ -0,0 +1,110 @@
+﻿using lib_comunicaciones.Interfaces;
+using lib_entidades.Modelos;
+using lib_presentaciones.Interfaces;
+using lib_utilidades;
+using System.Text.Json.Serialization;
+
+namespace lib_presentaciones.Implementaciones
+{
+    public class CargosPresentacion : ICargosPresentacion
+    {
+        private ICargosComunicacion? iComunicacion = null;
+
+        public CargosPresentacion(ICargosComunicacion iComunicacion)
+        {
+            this.iComunicacion = iComunicacion;
+        }
+
+        public async Task<List<Cargos>> Listar()
+        {
+            var lista = new List<Cargos>();
+            var datos = new Dictionary<string, object>();
+
+            var respuesta = await iComunicacion!.Listar(datos);
+            if (respuesta.ContainsKey("Error"))
+            {
+                throw new Exception(respuesta["Error"].ToString()!);
+            }
+            lista = JsonConversor.ConvertirAObjeto<List<Cargos>>(
+                JsonConversor.ConvertirAString(respuesta["Entidades"]));
+            return lista;
+        }
+
+        public async Task<List<Cargos>> Buscar(Cargos entidad, string tipo)
+        {
+            var lista = new List<Cargos>();
+            var datos = new Dictionary<string, object>();
+            datos["Entidad"] = entidad;
+            datos["Tipo"] = tipo;
+
+            var respuesta = await iComunicacion!.Buscar(datos);
+            if (respuesta.ContainsKey("Error"))
+            {
+                throw new Exception(respuesta["Error"].ToString()!);
+            }
+            lista = JsonConversor.ConvertirAObjeto<List<Cargos>>(
+                JsonConversor.ConvertirAString(respuesta["Entidades"]));
+            return lista;
+        }
+
+        public async Task<Cargos> Guardar(Cargos entidad)
+        {
+            if (entidad.Id != 0 || !entidad.Validar())
+            {
+                throw new Exception("lbFaltaInformacion");
+            }
+
+            var datos = new Dictionary<string, object>();
+            datos["Entidad"] = entidad;
+
+            var respuesta = await iComunicacion!.Guardar(datos);
+            if (respuesta.ContainsKey("Error"))
+            {
+                throw new Exception(respuesta["Error"].ToString()!);
+            }
+            entidad = JsonConversor.ConvertirAObjeto<Cargos>(
+                JsonConversor.ConvertirAString(respuesta["Entidad"]));
+            return entidad;
+        }
+
+        public async Task<Cargos> Modificar(Cargos entidad)
+        {
+            if (entidad.Id == 0 || !entidad.Validar())
+            {
+                throw new Exception("lbFaltaInformacion");
+            }
+
+            var datos = new Dictionary<string, object>();
+            datos["Entidad"] = entidad;
+
+            var respuesta = await iComunicacion!.Modificar(datos);
+            if (respuesta.ContainsKey("Error"))
+            {
+                throw new Exception(respuesta["Error"].ToString()!);
+            }
+            entidad = JsonConversor.ConvertirAObjeto<Cargos>(
+                JsonConversor.ConvertirAString(respuesta["Entidad"]));
+            return entidad;
+        }
+
+        public async Task<Cargos> Borrar(Cargos entidad)
+        {
+            if (entidad.Id == 0 || !entidad.Validar())
+            {
+                throw new Exception("lbFaltaInformacion");
+            }
+
+            var datos = new Dictionary<string, object>();
+            datos["Entidad"] = entidad;
+
+            var respuesta = await iComunicacion!.Borrar(datos);
+            if (respuesta.ContainsKey("Error"))
+            {
+                throw new Exception(respuesta["Error"].ToString()!);
+            }
+            entidad = JsonConversor.ConvertirAObjeto<Cargos>(
+                JsonConversor.ConvertirAString(respuesta["Entidad"]));
+            return entidad;
+        }
+    }
+}
---
+++ lib_presentaciones/Interfaces/ICargos_EstudiosPresentacion.cs
@@ -0,0 +1,13 @@
+﻿using lib_entidades.Modelos;
+
+namespace lib_presentaciones.Interfaces
+{
+    public interface ICargos_EstudiosPresentacion
+    {
+        Task<List<Cargos_Estudios>> Listar();
+        Task<List<Cargos_Estudios>> Buscar(Cargos_Estudios entidad, string tipo);
+        Task<Cargos_Estudios> Guardar(Cargos_Estudios entidad);
+        Task<Cargos_Estudios> Modificar(Cargos_Estudios entidad);
+        Task<Cargos_Estudios> Borrar(Cargos_Estudios entidad);
+    }
+}
