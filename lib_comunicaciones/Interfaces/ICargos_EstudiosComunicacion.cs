dc83e9f8e8ef831a07655f1fb5998e4605319877
sofiavilla18 <sofiavilla03@hotmail.com>
2024-11-07 16:41:03 -0500

    Actualizacion lib_comunicaciones

---
+++ lib_comunicaciones/Interfaces/ICargos_EstudiosComunicacion.cs
@@ -0,0 +1,17 @@
+﻿namespace lib_comunicaciones.Interfaces
+{
+    public interface ICargos_EstudiosComunicacion
+    {
+        //recibe peticiones y saca diccionarios
+        //task lo del hilo, permite que al ejecutar el codigo se bloquea seguir utilizandolo con el bug 
+        //hasta que ud no me responde no va a continuar, la parte grafica no se va a bloquear
+
+        //en la base de datos hay muchos registros por eso la funcion de listar()
+        //en la presnetacion 
+        Task<Dictionary<string, object>> Listar(Dictionary<string, object> datos);
+        Task<Dictionary<string, object>> Buscar(Dictionary<string, object> datos);
+        Task<Dictionary<string, object>> Guardar(Dictionary<string, object> datos);
+        Task<Dictionary<string, object>> Modificar(Dictionary<string, object> datos);
+        Task<Dictionary<string, object>> Borrar(Dictionary<string, object> datos);
+    }
+}
