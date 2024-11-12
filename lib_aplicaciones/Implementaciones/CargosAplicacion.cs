7c7342f1be62894f454c729253b444e16190e66c
sofiavilla18 <sofiavilla03@hotmail.com>
2024-11-12 09:39:41 -0500

    modificacion Cargos Aplicacion

--- lib_aplicaciones/Implementaciones/CargosAplicacion.cs
+++ lib_aplicaciones/Implementaciones/CargosAplicacion.cs
@@ -1,12 +1,80 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using lib_entidades;
+using lib_aplicaciones.Interfaces;
+using lib_repositorios.Interfaces;
+using System.Linq.Expressions;
+using lib_entidades.Modelos;
 
 namespace lib_aplicaciones.Implementaciones
 {
-    internal class CargosAplicacion
+    public class CargosAplicacion : ICargosAplicacion
     {
+        private ICargosRepositorio? iRepositorio = null;
+
+        public CargosAplicacion(ICargosRepositorio iRepositorio)
+        {
+            this.iRepositorio = iRepositorio;
+        }
+
+        public void Configurar(string string_conexion)
+        {
+            this.iRepositorio!.Configurar(string_conexion);
+        }
+
+        public Cargos Borrar(Cargos entidad)
+        {
+            if (entidad == null || !entidad.Validar())
+                throw new Exception("lbFaltaInformacion");
+
+            if (entidad.Id == 0)
+                throw new Exception("lbNoSeGuardo");
+
+            entidad = iRepositorio!.Borrar(entidad);
+            return entidad;
+        }
+
+        public Cargos Guardar(Cargos entidad)
+        {
+            if (entidad == null || !entidad.Validar())
+                throw new Exception("lbFaltaInformacion");
+
+            if (entidad.Id != 0)
+                throw new Exception("lbYaSeGuardo");
+
+            entidad = iRepositorio!.Guardar(entidad);
+            return entidad;
+        }
+
+        public List<Cargos> Listar()
+        {
+            return iRepositorio!.Listar();
+        }
+
+        public List<Cargos> Buscar(Cargos entidad, string tipo)
+        {
+            Expression<Func<Cargos, bool>>? condiciones = null;
+            switch (tipo.ToUpper())
+            {
+                case "NOMBRE": condiciones = x => x.Nombre!.Contains(entidad.Nombre!); break;
+                case "CODIGO ESTUDIO": condiciones = x => x.Cod_estudio == entidad.Cod_estudio; break;
+                case "COMPLEJA":
+                    condiciones =
+                        x => x.Nombre!.Contains(entidad.Nombre!) ||
+                             x.Cod_estudio == entidad.Cod_estudio; break;
+                default: condiciones = x => x.Id == entidad.Id; break;
+            }
+            return this.iRepositorio!.Buscar(condiciones);
+        }
+
+        public Cargos Modificar(Cargos entidad)
+        {
+            if (entidad == null || !entidad.Validar())
+                throw new Exception("lbFaltaInformacion");
+
+            if (entidad.Id == 0)
+                throw new Exception("lbNoSeGuardo");
+
+            entidad = iRepositorio!.Modificar(entidad);
+            return entidad;
+        }
     }
 }
---
+++ lib_aplicaciones/Interfaces/ICargosAplicacion.cs
@@ -0,0 +1,14 @@
+﻿using lib_entidades.Modelos;
+
+namespace lib_aplicaciones.Interfaces
+{
+    public interface ICargosAplicacion
+    {
+        void Configurar(string string_conexion);
+        List<Cargos> Buscar(Cargos entidad, string tipo);
+        List<Cargos> Listar();
+        Cargos Guardar(Cargos entidad);
+        Cargos Modificar(Cargos entidad);
+        Cargos Borrar(Cargos entidad);
+    }
+}
