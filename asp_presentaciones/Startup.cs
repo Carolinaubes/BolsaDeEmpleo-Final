2019c478742219f1ed414599203530a6381f7d32
Carolinaubes <carolinauribebuiles06@gmail.com>
2024-11-21 14:54:17 -0500

    Inyección de dependencias completa dentro del startup para asp_presentacion

--- asp_presentaciones/Startup.cs
+++ asp_presentaciones/Startup.cs
@@ -17,9 +17,28 @@ namespace asp_presentacion
         public void ConfigureServices(WebApplicationBuilder builder, IServiceCollection services)
         {
             // Comunicaciones
+            services.AddScoped<IVacantesComunicacion, VacantesComunicacion>();
+            services.AddScoped<IRolesComunicacion, RolesComunicacion>();
+            services.AddScoped<IPostulacionesComunicacion, PostulacionesComunicacion>();
+            services.AddScoped<IPersonas_EstudiosComunicacion, Personas_EstudiosComunicacion>();
+            services.AddScoped<IPersonasComunicacion, PersonasComunicacion>();
             services.AddScoped<IEstudiosComunicacion, EstudiosComunicacion>();
+            services.AddScoped<IEmpresasComunicacion, EmpresasComunicacion>();
+            services.AddScoped<ICargos_EstudiosComunicacion, Cargos_EstudiosComunicacion>();
+            services.AddScoped<ICargosComunicacion, CargosComunicacion>();
+            services.AddScoped<IAuditoriasComunicacion, AuditoriasComunicacion>(); //Este por si acaso
+
             // Presentaciones
+            services.AddScoped<IVacantesPresentacion, VacantesPresentacion>();
+            services.AddScoped<IRolesPresentacion, RolesPresentacion>();
+            services.AddScoped<IPostulacionesPresentacion, PostulacionesPresentacion>();
+            services.AddScoped<IPersonas_EstudiosPresentacion, Personas_EstudiosPresentacion>();
+            services.AddScoped<IPersonasPresentacion, PersonasPresentacion>();
             services.AddScoped<IEstudiosPresentacion, EstudiosPresentacion>();
+            services.AddScoped<IEmpresasPresentacion, EmpresasPresentacion>();
+            services.AddScoped<ICargos_EstudiosPresentacion, Cargos_EstudiosPresentacion>();
+            services.AddScoped<ICargosPresentacion, CargosPresentacion>();
+            services.AddScoped<IAuditoriasPresentacion, AuditoriasPresentacion>(); //Este por si acaso
 
             services.AddControllers();
             services.AddEndpointsApiExplorer();
