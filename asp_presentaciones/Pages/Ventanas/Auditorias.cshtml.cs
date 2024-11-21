b6ed0123c6b89fe947de519104f462b9b517528c
Carolinaubes <carolinauribebuiles06@gmail.com>
2024-11-21 15:12:00 -0500

    Paginas Auditorias asp_presentacion

--- asp_presentaciones/Pages/Ventanas/Auditorias.cshtml.cs
+++ asp_presentaciones/Pages/Ventanas/Auditorias.cshtml.cs
@@ -1,12 +1,53 @@
+using lib_entidades.Modelos;
+using lib_presentaciones.Interfaces;
+using lib_utilidades;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace asp_presentaciones.Pages.Ventanas
+namespace asp_presentacion.Pages.Ventanas
 {
     public class AuditoriasModel : PageModel
     {
-        public void OnGet()
+        private IAuditoriasPresentacion? iPresentacion = null;
+
+        public AuditoriasModel(IAuditoriasPresentacion iPresentacion)
         {
+            try
+            {
+                this.iPresentacion = iPresentacion;
+                Filtro = new Auditorias();
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
         }
+
+        [BindProperty] public Enumerables.Ventanas Accion { get; set; }
+        [BindProperty] public Auditorias? Actual { get; set; }
+        [BindProperty] public Auditorias? Filtro { get; set; }
+        [BindProperty] public List<Auditorias>? Lista { get; set; }
+
+        public virtual void OnGet() { OnPostBtRefrescar(); }
+
+        //Auditorias unicamente puede ser visualizada
+        public void OnPostBtRefrescar()
+        {
+            try
+            {
+                Filtro!.Nombre = Filtro!.Nombre ?? "";
+
+                Accion = Enumerables.Ventanas.Listas;
+                var task = this.iPresentacion!.Buscar(Filtro!, "NOMBRE");
+                task.Wait();
+                Lista = task.Result;
+                Actual = null;
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
     }
 }
