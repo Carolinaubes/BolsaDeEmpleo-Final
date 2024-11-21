7849fd1f525ecbf65977085889667362125f7fd3
Carolinaubes <carolinauribebuiles06@gmail.com>
2024-11-21 13:36:13 -0500

    Vistas para la presentación

--- asp_presentaciones/obj/Release/net8.0/asp_presentaciones.AssemblyInfo.cs
+++ asp_presentaciones/obj/Release/net8.0/asp_presentaciones.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("asp_presentaciones")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed551c5b81750bc8f5176570705f436f780ff4c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("asp_presentaciones")]
