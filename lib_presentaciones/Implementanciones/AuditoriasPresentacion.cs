6ca5b94aa7ea870c5885e6e1303510222437dd18
ErianSerna <erianserna1114297@correo.itm.edu.co>
2024-11-21 14:08:35 -0500

    Se añade el codigo de AuditoriasPresentacion - IAuditoriasPresentacion y se borran RolesPresentación - IRolesPresentación

--- lib_presentaciones/Implementanciones/AuditoriasPresentacion.cs
+++ lib_presentaciones/Implementanciones/AuditoriasPresentacion.cs
@@ -1,12 +1,110 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using lib_comunicaciones.Interfaces;
+using lib_entidades.Modelos;
+using lib_presentaciones.Interfaces;
+using lib_utilidades;
+using System.Text.Json.Serialization;
 
-namespace lib_presentaciones.Implementanciones
+namespace lib_presentaciones.Implementaciones
 {
-    internal class AuditoriasPresentaciones
+    public class AuditoriasPresentacion : IAuditoriasPresentacion
     {
+        private IAuditoriasComunicacion? iComunicacion = null;
+
+        public AuditoriasPresentacion(IAuditoriasComunicacion iComunicacion)
+        {
+            this.iComunicacion = iComunicacion;
+        }
+
+        public async Task<List<Auditorias>> Listar()
+        {
+            var lista = new List<Auditorias>();
+            var datos = new Dictionary<string, object>();
+
+            var respuesta = await iComunicacion!.Listar(datos);
+            if (respuesta.ContainsKey("Error"))
+            {
+                throw new Exception(respuesta["Error"].ToString()!);
+            }
+            lista = JsonConversor.ConvertirAObjeto<List<Auditorias>>(
+                JsonConversor.ConvertirAString(respuesta["Entidades"]));
+            return lista;
+        }
+
+        public async Task<List<Auditorias>> Buscar(Auditorias entidad, string tipo)
+        {
+            var lista = new List<Auditorias>();
+            var datos = new Dictionary<string, object>();
+            datos["Entidad"] = entidad;
+            datos["Tipo"] = tipo;
+
+            var respuesta = await iComunicacion!.Buscar(datos);
+            if (respuesta.ContainsKey("Error"))
+            {
+                throw new Exception(respuesta["Error"].ToString()!);
+            }
+            lista = JsonConversor.ConvertirAObjeto<List<Auditorias>>(
+                JsonConversor.ConvertirAString(respuesta["Entidades"]));
+            return lista;
+        }
+
+        public async Task<Auditorias> Guardar(Auditorias entidad)
+        {
+            if (entidad.Id != 0 || !entidad.Validar())
+            {
+                throw new Exception("lbFaltaInformacion");
+            }
+
+            var datos = new Dictionary<string, object>();
+            datos["Entidad"] = entidad;
+
+            var respuesta = await iComunicacion!.Guardar(datos);
+            if (respuesta.ContainsKey("Error"))
+            {
+                throw new Exception(respuesta["Error"].ToString()!);
+            }
+            entidad = JsonConversor.ConvertirAObjeto<Auditorias>(
+                JsonConversor.ConvertirAString(respuesta["Entidad"]));
+            return entidad;
+        }
+
+        public async Task<Auditorias> Modificar(Auditorias entidad)
+        {
+            if (entidad.Id == 0 || !entidad.Validar())
+            {
+                throw new Exception("lbFaltaInformacion");
+            }
+
+            var datos = new Dictionary<string, object>();
+            datos["Entidad"] = entidad;
+
+            var respuesta = await iComunicacion!.Modificar(datos);
+            if (respuesta.ContainsKey("Error"))
+            {
+                throw new Exception(respuesta["Error"].ToString()!);
+            }
+            entidad = JsonConversor.ConvertirAObjeto<Auditorias>(
+                JsonConversor.ConvertirAString(respuesta["Entidad"]));
+            return entidad;
+        }
+
+        public async Task<Auditorias> Borrar(Auditorias entidad)
+        {
+            if (entidad.Id == 0 || !entidad.Validar())
+            {
+                throw new Exception("lbFaltaInformacion");
+            }
+
+            var datos = new Dictionary<string, object>();
+            datos["Entidad"] = entidad;
+
+            var respuesta = await iComunicacion!.Borrar(datos);
+            if (respuesta.ContainsKey("Error"))
+            {
+                throw new Exception(respuesta["Error"].ToString()!);
+            }
+            entidad = JsonConversor.ConvertirAObjeto<Auditorias>(
+                JsonConversor.ConvertirAString(respuesta["Entidad"]));
+            return entidad;
+        }
     }
 }
